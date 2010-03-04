bf48c69ca8d7608be49887d08d59dfc43eec1540
Chris Parsons and Enrique Comba Riepenhausen <dev+chrismdp+ecomba@edendevelopment.co.uk>
2010-03-04 23:45:21 +0000

    More on receiving push

--- pushy/receive.py
+++ pushy/receive.py
@@ -1,9 +1,26 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+import urllib
+
+def _extract_wave_id(url):
+	_split = url.split('/')
+	if (len(_split) < 3):
+		return ""
+	return urllib.unquote(_split[2])
+
+def _generate_wavelet_id_from_wave_id(wave_id):
+	return "%s!conv+root" % wave_id.split('!')[0]
+
 class PushHandler(webapp.RequestHandler):
 	def get(self):
-		self.response.out.write("HI")
+		wave_id = _extract_wave_id(self.request.path)
+		if (wave_id == ""):
+			self.response.out.write("Malformed URL")
+			return
+		wavelet_id = _generate_wavelet_id_from_wave_id(wave_id)
+		#_robot.fetch_wave(wave_id, wavelet_id)
+		self.response.out.write("OK")
 		
 def main():
 	application = webapp.WSGIApplication([('/push.*', PushHandler)], debug=True)
--- test/test_running_robot.py
+++ test/test_running_robot.py
@@ -4,34 +4,40 @@ from webtest import TestApp
 from waveapi import robot
 from waveapi import appengine_robot_runner
 from pushy import receive
+from pushy import test
 
 from google.appengine.ext import webapp
-
-def assertContains(self, itemToLookFor, itemToSearch):
-	def escapeHtml(string):
-		return string.replace("<", "&lt;")
-
-	escapedItemToLookFor = escapeHtml(itemToLookFor)
-	escapedItemToSearch = escapeHtml(str(itemToSearch))
-	self.assertTrue(itemToLookFor in itemToSearch, "%s did not contain %s" % (escapedItemToSearch, escapedItemToLookFor))
 
 class TestReceivePush(unittest.TestCase):
 	def setUp(self):
 		self._application = webapp.WSGIApplication([('/push.*', receive.PushHandler)], debug=True)
+		self._app = TestApp(self._application)
 
-	def testRespondsToPush(self):
-		app = TestApp(self._application)
-		response = app.get('/push')
-		self.assertEquals('200 OK', response.status)
+	def assertContains(self, itemToLookFor, itemToSearch):
+		def escapeHtml(string):
+			return string.replace("<", "&lt;")
 
-#class TestRunningRobot(unittest.TestCase):
-#  def setUp(self):
-#		self._robot = robot.Robot("Test Pushy")
-#		self._application = appengine_robot_runner.create_robot_webapp(self._robot)
+		escapedItemToLookFor = escapeHtml(itemToLookFor)
+		escapedItemToSearch = escapeHtml(str(itemToSearch))
+		self.assertTrue(itemToLookFor in itemToSearch, "%s did not contain %s" % (escapedItemToSearch, escapedItemToLookFor))
 
-#	def test_running_tests(self):
-#		app = TestApp(self._application)
-#		response = app.get("/_wave/capabilities.xml")
-#		self.assertEquals("200 OK", response.status)
+	def testPostsToWaveOnPush(self):
+		response = self._app.get('/push/wavesandbox.com!F+33934934')
+		self.assertContains('OK', response)
+	
+	def testMalformedRequestShouldFail(self):
+		response = self._app.get('/push')
+		self.assertContains("Malformed URL", response)
 
+class TestReceive(unittest.TestCase):
+	def testExtractWaveId(self):
+		id = receive._extract_wave_id("/push/wavesandbox.com%21w%2B27uuNu-2A")
+		self.assertEquals("wavesandbox.com!w+27uuNu-2A", id)
+
+	def testExtractWaveIdReturnsEmptyOnMalformedLink(self):
+		id = receive._extract_wave_id("/push")
+		self.assertEquals("", id)
+
+	def testGenerateWaveletIdFromWaveId(self):
+		self.assertEquals("wavesandbox.com!conv+root", receive._generate_wavelet_id_from_wave_id("wavesandbox.com!w+27uuNu-2A"))
 
